dd670252aef342f52ae8c98126deb51e88b44e9f
Pablo Bobarini <50304993+PablissimoB@users.noreply.github.com>
2023-08-29 18:01:16 -0300

    Add project files.

---
+++ clase01/WebForm1.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="clase01.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+            <asp:Label ID="Label1" runat="server" Text="Apellido"></asp:Label>
+            &nbsp;<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            
+            <asp:Label ID="Label2" runat="server" Text="Nombre"></asp:Label>
+            &nbsp;<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <asp:RadioButton ID="RadioButton1" runat="server" Text="Casado/a" GroupName="rdButtons" />
+            <asp:RadioButton ID="RadioButton2" runat="server" Text="Soltero/a" GroupName="rdButtons"/>
+            <asp:RadioButton ID="RadioButton3" runat="server" Text="Viudo/a" GroupName="rdButtons"/>
+            <br />
+        </div>
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+            <asp:ListItem Value="ncn">nacional</asp:ListItem>
+            <asp:ListItem Value="ext">extranjero</asp:ListItem>
+        </asp:RadioButtonList>
+        <br />
+        <asp:CheckBox ID="CheckBox1" runat="server" Text="Secundaria" />
+        <asp:CheckBox ID="CheckBox2" runat="server" Text="Terciaria" />
+        <asp:CheckBox ID="CheckBox3" runat="server" Text="Universitaria" />
+        <br />
+        <br />
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:ListBox ID="ListBox1" runat="server" Width="104px"></asp:ListBox>
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    </form>
+</body>
+</html>
---
+++ clase01/WebForm1.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace clase01
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DropDownList1.Items.Add(new ListItem("Argentina", "arg"));
+                DropDownList1.Items.Add(new ListItem("Chile", "chl"));
+            }
+
+        }
+
+        public string getNivelEducativo()
+        {
+            string nivelEducativo = "";
+            if (CheckBox1.Checked)
+            {
+                nivelEducativo+= " "+ CheckBox1.Text;
+            }
+            if (CheckBox2.Checked)
+            {
+                nivelEducativo += " " + CheckBox2.Text;
+            }
+            nivelEducativo += CheckBox3.Checked ? " " + CheckBox3.Text:"";
+
+            return nivelEducativo;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text !="" && TextBox2.Text !="" && RadioButtonList1.SelectedIndex >=0)
+            {
+                Label3.Text = $"Nombre: {TextBox2.Text}, " +
+                    $"Apellido: {TextBox1.Text}," +
+                    $"{RadioButtonList1.SelectedItem.Text}," +
+                    $"nivel educativo: {getNivelEducativo()}";
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string codPais = DropDownList1.SelectedValue;
+            ListBox1.Items.Clear();
+            switch(codPais)
+            {
+                case "arg":
+                    ListBox1.Items.Add(new ListItem("Cordoba", "cba"));
+                    ListBox1.Items.Add(new ListItem("CABA", "caba"));
+                    break;
+                case "chl":
+                    ListBox1.Items.Add(new ListItem("Santiago", "sgo"));
+                    break;
+            }
+
+        }
+    }
+}
